fbd004b29c9cde6f58e33bfcb49a732be05c5e96
cabra <18071869608@163.com>
2019-07-08 14:53:12 +0800

    nothing

--- src/components/common/Home.vue
+++ src/components/common/Home.vue
@@ -64,6 +64,7 @@
             // 只有在标签页列表里的页面才使用keep-alive，即关闭标签之后就不保存到内存中了。
             bus.$on('tags', msg => {
                 let arr = [];
+                let me =hello;
                 for(let i = 0, len = msg.length; i < len; i ++){
                     if(msg[i].name=='valuetable'){}//剔除keepalive
                     else
